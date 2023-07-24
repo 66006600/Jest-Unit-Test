bed11829b851aa4b704d2b63e8966cf879400dca
Beverly M <bevponi@gmail.com>
2023-07-24 23:52:09 +0000

    tratando

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const sum = (a,b) => {
-    return a + b
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
 }
 
-console.log(sum(7,3))
-module.exports = { sum };
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
 
+function fromDollarToYen(dollars) {
+    // Convertir dólares a yenes japoneses
+    return dollars * oneEuroIs["JPY"] / oneEuroIs["USD"];
+}
 
+function fromEuroToDollar(euros) {
+    // Convertir euros a dólares estadounidenses
+    return euros * oneEuroIs["USD"];
+}
+
+function fromYenToPound(yen) {
+    // Convertir yenes japoneses a Libras esterlinas
+    return yen * oneEuroIs["GBP"] / oneEuroIs["JPY"];
+}
+
+module.exports = {
+    fromDollarToYen,
+    fromEuroToDollar,
+    fromYenToPound,
+};
