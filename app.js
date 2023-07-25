00bcba7c54ff44dce4216a3213ab4978ad057286
Beverly M <bevponi@gmail.com>
2023-07-25 00:22:40 +0000

    Punto 9

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-const fromEuroToDollar = function(valueInEuro){
-    // convertimos el valor a dolares
-    let valueInDollar = valueInEuro * 1.2;
-    // retornamos el valor
-    return valueInDollar;
-}
-
 let oneEuroIs = {
     "JPY": 127.9, // japan yen
     "USD": 1.2, // us dollar
     "GBP": 0.8, // british pound
-}
+};
 
-function fromDollarToYen(dollars) {
-    // Convertir dólares a yenes japoneses
+function fromDollarToYen(dollars) { 
     return dollars * oneEuroIs["JPY"] / oneEuroIs["USD"];
 }
 
 function fromEuroToDollar(euros) {
-    // Convertir euros a dólares estadounidenses
     return euros * oneEuroIs["USD"];
 }
 
 function fromYenToPound(yen) {
-    // Convertir yenes japoneses a Libras esterlinas
     return yen * oneEuroIs["GBP"] / oneEuroIs["JPY"];
 }
 
